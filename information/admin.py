fa2ae4e83274252c341b622c0e4e6c96af86bea9
Sayed Fikri <sayedariffikri@gmail.com>
2020-04-26 22:23:41 +0800

    create page and annoucement model

--- information/admin.py
+++ information/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Page, Annoucement
+
+class PageAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',)
+
+class AnnoucementAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',)
+
+admin.site.register(Page, PageAdmin)
+admin.site.register(Annoucement, AnnoucementAdmin)
 
 # Register your models here.
--- information/models.py
+++ information/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Page(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    url = models.URLField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    # important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Annoucement(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    # important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
